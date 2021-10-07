fd88ab50274a07e47ec68bca997a06a56c0b1243
Trevor Barnes <trevor_barnes@sfu.ca>
2021-10-07 06:30:57 -0700

    Updated snakefile so default_values triggers cascading updates

--- src/osemosys_global/OPG_file_check.py
+++ src/osemosys_global/OPG_file_check.py
@@ -19,19 +19,17 @@ output_dir = parsed_yaml_file.get('outputDir') + 'data/'
 def create_csv_files(path):
     csv_file_dict = {} 
     for each_csv in os.listdir(r'../../simplicity/data/'):  
-        if each_csv not in os.listdir(path):
-            #copy default values csv with data
-            if each_csv == "default_values.csv":
+        #copy default values csv with data
+        if each_csv == "default_values.csv":
                 shutil.copy(r'../../data/default_values.csv',path)
-            #copy file names and column headers only
-            else:
-                csv_df_in = pd.read_csv(os.path.join(r'../../simplicity/data/',
+        if each_csv not in os.listdir(path):
+            csv_df_in = pd.read_csv(os.path.join(r'../../simplicity/data/',
                                                   each_csv))
-                csv_df_out = pd.DataFrame(columns = list(csv_df_in.columns))
-                csv_df_out.to_csv(os.path.join(path, 
+            csv_df_out = pd.DataFrame(columns = list(csv_df_in.columns))
+            csv_df_out.to_csv(os.path.join(path, 
                                                each_csv),
                                  index = None)
-                csv_file_dict[each_csv] = list(csv_df_in.columns)
+            csv_file_dict[each_csv] = list(csv_df_in.columns)
     
     return csv_file_dict
 
