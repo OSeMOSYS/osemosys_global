86215ff27e7814dfbe85ac27f311efacf40a952f
trevorb1 <trevor_barnes@sfu.ca>
2023-04-17 20:52:07 -0700

    update filecheckq

--- workflow/scripts/osemosys_global/file_check.py
+++ workflow/scripts/osemosys_global/file_check.py
@@ -3,24 +3,26 @@
 
 import pandas as pd
 import os
-import shutil
+import sys
 from osemosys_global.configuration import ConfigPaths
 import logging 
 logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.INFO)
 
-
-# CONFIGURATION PARAMETERS
-config_paths = ConfigPaths()
-input_data_dir = config_paths.input_data_dir
-output_data_dir = config_paths.output_data_dir
-otoole_data = os.path.join(config_paths.otoole, "data")
-
-# File Check logic
-
-for each_csv in os.listdir(otoole_data):
-    if each_csv not in os.listdir(output_data_dir): 
-        csv_df_in = pd.read_csv(os.path.join(otoole_data, each_csv))
-        csv_df_out = pd.DataFrame(columns = list(csv_df_in.columns))
-        csv_df_out.to_csv(os.path.join(output_data_dir, each_csv), index = None)
+def main(output_data: str, otoole_data: str):
+    """Checks for missing otoole files"""
+    for each_csv in os.listdir(otoole_data):
+        if each_csv not in os.listdir(output_data): 
+            csv_df_in = pd.read_csv(os.path.join(otoole_data, each_csv))
+            csv_df_out = pd.DataFrame(columns = list(csv_df_in.columns))
+            csv_df_out.to_csv(os.path.join(output_data, each_csv), index = None)
     
-logging.info('File Check Completed')
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Usage: python demand_projection.py <results/data> <resources/otoole> ")
+        logging.info('File Check Failed')
+    else:
+        main(
+            sys.argv[1], 
+            sys.argv[2], 
+        )
+        logging.info('File Check Completed')
