37c028327759ab480cca139e708c5f70ec489bc6
trevorb1 <trevor_barnes@sfu.ca>
2023-04-22 17:49:35 -0700

    geographic filter update

--- workflow/scripts/osemosys_global/file_check.py
+++ workflow/scripts/osemosys_global/file_check.py
@@ -4,21 +4,24 @@
 import pandas as pd
 import os
 import sys
-from osemosys_global.configuration import ConfigPaths
+from pathlib import Path
 import logging 
 logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.INFO)
 
-def main(output_data: str, otoole_data: str):
+def main(data: str, otoole_data: str):
     """Checks for missing otoole files"""
-    for each_csv in os.listdir(otoole_data):
-        if each_csv not in os.listdir(output_data): 
-            csv_df_in = pd.read_csv(os.path.join(otoole_data, each_csv))
+    DATA = Path(data)
+    OTOOLE = Path(otoole_data)
+    
+    for each_csv in os.listdir(OTOOLE):
+        if each_csv not in os.listdir(DATA): 
+            csv_df_in = pd.read_csv(Path(OTOOLE, each_csv))
             csv_df_out = pd.DataFrame(columns = list(csv_df_in.columns))
-            csv_df_out.to_csv(os.path.join(output_data, each_csv), index = None)
+            csv_df_out.to_csv(Path(DATA, each_csv), index = None)
     
 if __name__ == "__main__":
     if len(sys.argv) != 3:
-        print("Usage: python demand_projection.py <results/data> <resources/otoole> ")
+        print("Usage: python file_check.py.py <input/csv/dir/> </otoole/csv/dir/> ")
         logging.info('File Check Failed')
     else:
         main(
--- workflow/scripts/osemosys_global/geographic_filter.py
+++ workflow/scripts/osemosys_global/geographic_filter.py
@@ -5,93 +5,111 @@
 
 import pandas as pd
 import os
-import yaml
-import shutil
+import sys
 from pathlib import Path
+from typing import List
 from osemosys_global.configuration import ConfigFile, ConfigPaths
+from osemosys_global.utils import get_config_data
 import logging 
 logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.INFO)
 
-# CONFIGURATION PARAMETERS
+def main(input_data: str, output_data: str, geographic_scope: List[str], nodes_to_remove: List[str]):
+    """Filters for scenario geography 
+    
+    Args:
+        input_data: str
+            Path to resources/data folder 
+        output_dir: str
+            Path to results/data folder
+        geographic_scope: List[str]
+            Start year of demand 
+        nodes_to_remove: List[str]
+            Nodes to remove from the model
+    """
+    
+    # set path variables
+    INPUT_DATA_DIR = Path(input_data)
+    OUTPUT_DATA_DIR = Path(output_data)
+    
+    if not geographic_scope: # Check for empty list (ie. World run)
+        geographic_scope = []
+    geographic_scope.append('INT') # 'INT' for international fuels added by default
+    international_fuels = ['COA', 'COG', 'GAS', 'OIL', 'PET', 'OTH', 'URN']
 
-config_paths = ConfigPaths()
-config = ConfigFile('config')  
+    if not os.path.exists(OUTPUT_DATA_DIR):
+        os.makedirs(OUTPUT_DATA_DIR)
 
-scenario_name = config.get('scenario')
-geographic_scope = config.get('geographic_scope')
-remove_nodes = config.get('nodes_to_remove')
+    for each_csv in Path(INPUT_DATA_DIR).glob('*.csv'):
+        df = pd.read_csv(os.path.join(INPUT_DATA_DIR, each_csv))
 
-output_data_dir = config_paths.output_data_dir
-scenario_dir = config_paths.scenario_dir
-scenario_data_dir = config_paths.scenario_data_dir
+        if not df.empty:
+            # Do not filter if only element is international fuels
+            if geographic_scope[0] != 'INT': 
+                if 'TECHNOLOGY' in df.columns:
+                    df = df.loc[df['TECHNOLOGY'].str[3:6].isin(geographic_scope) | 
+                                df['TECHNOLOGY'].str[6:9].isin(geographic_scope) | 
+                                df['TECHNOLOGY'].str[8:11].isin(geographic_scope)]
 
-# FILTERING 
+                    # Filter out all international TRN techs 
+                    df = df.loc[~(
+                        df['TECHNOLOGY'].str.startswith('TRN') &
+                        (~(df['TECHNOLOGY'].str[3:6].isin(geographic_scope)) |
+                        ~(df['TECHNOLOGY'].str[8:11].isin(geographic_scope)))
+                        )]
 
-if not geographic_scope: # Check for empty list (ie. World run)
-    geographic_scope = []
-geographic_scope.append('INT') # 'INT' for international fuels added by default
-international_fuels = ['COA', 'COG', 'GAS', 'OIL', 'PET', 'OTH', 'URN']
+                    if nodes_to_remove:
+                        df = df.loc[~(df['TECHNOLOGY'].str[3:8].isin(nodes_to_remove) | 
+                                    df['TECHNOLOGY'].str[6:11].isin(nodes_to_remove) | 
+                                    df['TECHNOLOGY'].str[8:13].isin(nodes_to_remove))]
 
-if not os.path.exists(scenario_data_dir):
-    os.makedirs(scenario_data_dir)
+                if 'FUEL' in df.columns:
+                    df = df.loc[df['FUEL'].str[3:6].isin(geographic_scope) | 
+                                df['FUEL'].str[6:9].isin(geographic_scope) |
+                                df['FUEL'].isin(international_fuels)]
+                    
+                    if nodes_to_remove:    
+                        df = df.loc[~(df['FUEL'].str[3:8].isin(nodes_to_remove) | 
+                                    df['FUEL'].str[6:11].isin(nodes_to_remove))]
 
-for each_csv in Path(output_data_dir).glob('*.csv'):
-    df = pd.read_csv(os.path.join(output_data_dir, each_csv))
+                if str(each_csv).split('/')[-1] == 'FUEL.csv':
+                    df = df.loc[df['VALUE'].str[3:6].isin(geographic_scope) | 
+                                df['VALUE'].str[6:9].isin(geographic_scope) |
+                                df['VALUE'].isin(international_fuels)]
+                    
+                    if nodes_to_remove:
+                        df = df.loc[~(df['VALUE'].str[3:8].isin(nodes_to_remove) | 
+                                    df['VALUE'].str[6:11].isin(nodes_to_remove))]
 
-    if not df.empty:
-        # Do not filter if only element is international fuels
-        if geographic_scope[0] != 'INT': 
-            if 'TECHNOLOGY' in df.columns:
-                df = df.loc[df['TECHNOLOGY'].str[3:6].isin(geographic_scope) | 
-                            df['TECHNOLOGY'].str[6:9].isin(geographic_scope) | 
-                            df['TECHNOLOGY'].str[8:11].isin(geographic_scope)]
+                if str(each_csv).split('/')[-1] == 'TECHNOLOGY.csv':
+                    df = df.loc[df['VALUE'].str[3:6].isin(geographic_scope) | 
+                                df['VALUE'].str[6:9].isin(geographic_scope) | 
+                                df['VALUE'].str[8:11].isin(geographic_scope)]
+                    df = df.loc[~(
+                        df['VALUE'].str.startswith('TRN') &
+                        (~(df['VALUE'].str[3:6].isin(geographic_scope)) |
+                        ~(df['VALUE'].str[8:11].isin(geographic_scope)))
+                        )]
 
-                # Filter out all international TRN techs 
-                df = df.loc[~(
-                    df['TECHNOLOGY'].str.startswith('TRN') &
-                    (~(df['TECHNOLOGY'].str[3:6].isin(geographic_scope)) |
-                    ~(df['TECHNOLOGY'].str[8:11].isin(geographic_scope)))
-                    )]
+                    if nodes_to_remove:
+                        df = df.loc[~(df['VALUE'].str[3:8].isin(nodes_to_remove) | 
+                                    df['VALUE'].str[6:11].isin(nodes_to_remove) | 
+                                    df['VALUE'].str[8:13].isin(nodes_to_remove))]
+            
+        df.to_csv(os.path.join(os.path.join(scenario_data_dir, each_csv.name)), index = None)
+        df.to_csv(Path(OUTPUT_DIR, each_csv.name), index = None)
 
-                if remove_nodes:
-                    df = df.loc[~(df['TECHNOLOGY'].str[3:8].isin(remove_nodes) | 
-                                df['TECHNOLOGY'].str[6:11].isin(remove_nodes) | 
-                                df['TECHNOLOGY'].str[8:13].isin(remove_nodes))]
-
-            if 'FUEL' in df.columns:
-                df = df.loc[df['FUEL'].str[3:6].isin(geographic_scope) | 
-                            df['FUEL'].str[6:9].isin(geographic_scope) |
-                            df['FUEL'].isin(international_fuels)]
-                
-                if remove_nodes:    
-                    df = df.loc[~(df['FUEL'].str[3:8].isin(remove_nodes) | 
-                                df['FUEL'].str[6:11].isin(remove_nodes))]
-
-            if str(each_csv).split('/')[-1] == 'FUEL.csv':
-                df = df.loc[df['VALUE'].str[3:6].isin(geographic_scope) | 
-                            df['VALUE'].str[6:9].isin(geographic_scope) |
-                            df['VALUE'].isin(international_fuels)]
-                
-                if remove_nodes:
-                    df = df.loc[~(df['VALUE'].str[3:8].isin(remove_nodes) | 
-                                df['VALUE'].str[6:11].isin(remove_nodes))]
-
-            if str(each_csv).split('/')[-1] == 'TECHNOLOGY.csv':
-                df = df.loc[df['VALUE'].str[3:6].isin(geographic_scope) | 
-                            df['VALUE'].str[6:9].isin(geographic_scope) | 
-                            df['VALUE'].str[8:11].isin(geographic_scope)]
-                df = df.loc[~(
-                    df['VALUE'].str.startswith('TRN') &
-                    (~(df['VALUE'].str[3:6].isin(geographic_scope)) |
-                    ~(df['VALUE'].str[8:11].isin(geographic_scope)))
-                    )]
-
-                if remove_nodes:
-                    df = df.loc[~(df['VALUE'].str[3:8].isin(remove_nodes) | 
-                                df['VALUE'].str[6:11].isin(remove_nodes) | 
-                                df['VALUE'].str[8:13].isin(remove_nodes))]
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: python geographic_filter.py <input/csv/dir> <output/csv/dir/> <config.yaml>")
+        logging.info('Geographic Filter Completed')
+    else:
+        config_values = ["geographic_scope", "nodes_to_remove"]
+        config_data = get_config_data(sys.argv[3], config_values)
         
-    df.to_csv(os.path.join(os.path.join(scenario_data_dir, each_csv.name)), index = None)
-
-
-logging.info('Geographic Filter Applied')
+        main(
+            sys.argv[1], 
+            sys.argv[2], 
+            config_data["geographic_scope"],
+            config_data["nodes_to_remove"],
+        )
+        logging.info('Geographic Filter Completed')
