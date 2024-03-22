bd9b69f3588c74a37eaf28f6947214aa61552b5d
trevorb1 <trevor_barnes@sfu.ca>
2024-03-22 15:24:43 -0700

    plotting fix

--- workflow/scripts/osemosys_global/visualise.py
+++ workflow/scripts/osemosys_global/visualise.py
@@ -123,7 +123,7 @@ def plot_generation_annual(data: Dict[str,pd.DataFrame], save_dir: str, country:
             system level
     """
 
-    df = get_generation_annual_data(data, country=None)
+    df = get_generation_annual_data(data, country=country)
     plot_colors = get_color_codes()
 
     if not country: # System level titles
