057efd1d7860fa0b310f99a692858e5ad07b3270
trevorb1 <trevor_barnes@sfu.ca>
2024-10-25 15:24:08 -0700

    add generation shares

---
+++ workflow/scripts/osemosys_global/summary/constants.py
@@ -0,0 +1,5 @@
+"""Constants for summary statistics"""
+
+RENEWABLES = ["GEO", "HYD", "SPV", "CSP", "WAS", "WAV", "WON", "WOF"]
+CLEAN = ["BIO", "GEO", "HYD", "SPV", "CSP", "WAS", "WAV", "WON", "WOF", "URN"]
+FOSSIL = ["CCG", "COA", "COG", "OCG", "OIL", "OTH", "PET"]
---
+++ workflow/scripts/osemosys_global/summary/gen_shares.py
@@ -0,0 +1,77 @@
+"""Calcualtes Renewable Generation
+
+Since emissions are reported at country levels, carbon intensities are also 
+reported at country levels. 
+"""
+
+import pandas as pd
+from typing import Optional
+from constants import CLEAN, RENEWABLES, FOSSIL
+
+
+def _get_gen_by_node(
+    production: pd.DataFrame, carriers: Optional[list[str]] = None
+) -> pd.DataFrame:
+
+    df = production.copy()
+
+    assert "TECHNOLOGY" in df.index.names
+
+    df["TECH"] = df.index.get_level_values("TECHNOLOGY").str[3:6]
+    df["NODE"] = df.index.get_level_values("TECHNOLOGY").str[6:11]
+
+    if carriers:
+        df = df[df.TECH.isin(carriers)]
+
+    return (
+        df.reset_index()[["REGION", "NODE", "YEAR", "VALUE"]]
+        .groupby(["REGION", "NODE", "YEAR"])
+        .sum()
+    )
+
+
+def calc_generation_shares(production_by_technology: pd.DataFrame) -> pd.DataFrame:
+
+    df = production_by_technology.copy()
+
+    df = df[
+        (df.index.get_level_values("TECHNOLOGY").str.startswith("PWR"))
+        & ~(df.index.get_level_values("TECHNOLOGY").str.contains("TRN"))
+    ]
+
+    total = _get_gen_by_node(df).rename(columns={"VALUE": "TOTAL"})
+    clean = _get_gen_by_node(df, CLEAN).rename(columns={"VALUE": "CLEAN"})
+    renewable = _get_gen_by_node(df, RENEWABLES).rename(columns={"VALUE": "RENEWABLE"})
+    fossil = _get_gen_by_node(df, FOSSIL).rename(columns={"VALUE": "FOSSIL"})
+
+    shares = (
+        total.join(clean, how="outer")
+        .join(renewable, how="outer")
+        .join(fossil, how="outer")
+        .fillna(0)
+    )
+
+    shares["CLEAN"] = shares.CLEAN.div(shares.TOTAL).mul(100)
+    shares["RENEWABLE"] = shares.RENEWABLE.div(shares.TOTAL).mul(100)
+    shares["FOSSIL"] = shares.FOSSIL.div(shares.TOTAL).mul(100)
+
+    return shares[["CLEAN", "RENEWABLE", "FOSSIL"]].round(1)
+
+
+if __name__ == "__main__":
+    if "snakemake" in globals():
+        production_by_technology_annual_csv = snakemake.input.production_by_technology
+        save = snakemake.output.generation_shares
+    else:
+        production_by_technology_annual_csv = (
+            "results/India/results/ProductionByTechnologyAnnual.csv"
+        )
+        save = "results/India/results/GenerationShares.csv"
+
+    production_by_technology_annual = pd.read_csv(
+        production_by_technology_annual_csv, index_col=[0, 1, 2, 3]
+    )
+
+    df = calc_generation_shares(production_by_technology_annual)
+
+    df.to_csv(save, index=True)
