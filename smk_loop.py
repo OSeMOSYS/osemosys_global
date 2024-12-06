dc75157b8443bc137179ba2a0e359a86e4c7c7e6
maartenbrinkerink <65602545+maartenbrinkerink@users.noreply.github.com>
2024-12-06 16:59:11 -0500

    Added snakemake loop

---
+++ smk_loop.py
@@ -0,0 +1,47 @@
+import subprocess
+import os
+import shutil
+
+config_dir = 'config_consecutive'
+data_dir = 'results/data'
+
+'''Set to 'list' to only run scenarios in 'scenario_list' or set to 'folder' to run all
+scenario config files as defined in 'config_dir'. Can be used to run a scenario sample.'''
+run_type = 'list'
+
+scenario_list = [
+    'trn15',
+    #'trn16',
+    'trn17',
+    #'trn18',
+    #'trn19',
+    #'trn20',
+    #'trn21',
+    #'trn22',
+    #'trn23',
+    #'trn24',
+    #'trn25',
+    #'trn26',
+    #'trn27',
+    #'trn28',
+    #'trn29',
+    #'trn30',
+    #'trn31',
+    #'trn32',
+    #'trn33',
+    #'trn34',
+    ]
+
+if run_type == 'folder':
+    for scenario in os.listdir(config_dir):
+        subprocess.run("snakemake --configfile "f'{config_dir}/{scenario}'"", 
+                       shell = True)
+        
+        shutil.rmtree(data_dir)
+        
+if run_type == 'list':
+    for scenario in scenario_list:
+        subprocess.run("snakemake --configfile "f'{config_dir}/{scenario}.yaml'"", 
+                       shell = True)
+        
+        shutil.rmtree(data_dir)
