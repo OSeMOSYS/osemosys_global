14ec2cb738868c6a75326543cb73e3f9920f329e
maartenbrinkerink <65602545+maartenbrinkerink@users.noreply.github.com>
2025-01-07 20:18:28 -0500

    Fuel prices set to global benchmarks

--- smk_loop.py
+++ smk_loop.py
@@ -2,7 +2,7 @@ import subprocess
 import os
 import shutil
 
-config_dir = 'config_consecutive'
+config_dir = 'config_consecutive/3hourly2seasons'
 data_dir = 'results/data'
 
 '''Set to 'list' to only run scenarios in 'scenario_list' or set to 'folder' to run all
